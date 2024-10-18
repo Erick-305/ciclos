f1dbf2d23fcd31125a8b047e5e660e55ea823c84
Erick Mejia <ericks.mejia2005@gmail.com>
2024-10-17 20:41:17 -0500

    ciclos javascript

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+document.write("Hola Mundo");
+document.write("Hola Mundo");
+document.write("Hola Mundo" + "<br>");
+//For es un bucle que tiene 3 parametros
+//La funcion es repetir un bloque de codigo las veces que quieras
+
+//sintaxis basica
+for(i=3;i<=15;i++){
+    document.write("Hola desde js" + "<br>");
+    if(i%2==0){
+        document.write(i+"<br>");
+    }
+}
